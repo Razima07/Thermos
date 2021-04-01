5fd3d27f9323ac87dc6cc37708a784c89d92a185
sysadmin <sysadmin@bcalab1-m09>
2021-04-01 09:44:50 +0530

    push

---
+++ practise/src/practise/Thermos.java
@@ -0,0 +1,28 @@
+package practise;
+
+public class Thermos {
+ String brand;
+ String liquid;
+ float qty;
+ 
+  Thermos(String liquid, float qty, String brand) {
+	 
+	
+ }
+ void add(float val) {
+	 qty = qty + val;
+ }
+ 
+ void displaydetails() {
+	 System.out.println("brand is: "+ brand);
+	 System.out.println("liquid is: "+ liquid);
+	 System.out.println("qty is: "+ qty);
+	
+ }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+  
+	}
+
+}
